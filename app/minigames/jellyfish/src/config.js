eee3101fe3fe13f11d3a0f64de563f77bcb2f425
Cyprien <cyprien.larrouy@gmail.com>
2016-12-25 19:37:18 +0100

    config change for debug

--- app/minigames/jellyfish/src/config.js
+++ app/minigames/jellyfish/src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     version : 1.1,
     gameId: "jellyfish",     // the module build will export to this foldername. lowercase for the filesystem
     discipline: "maths",
-    skipKalulu : true, // do not play kalulu speeches for faster debug. incomplete implementation
+    skipKalulu : false, // do not play kalulu speeches for faster debug. incomplete implementation
     skipKaluluIntro : false, // do not play kalulu speeches for faster debug. incomplete implementation
     skipKaluluHelp : false, // do not play kalulu speeches for faster debug. incomplete implementation
     skipKaluluFinal : false, // do not play kalulu speeches for faster debug. incomplete implementation
