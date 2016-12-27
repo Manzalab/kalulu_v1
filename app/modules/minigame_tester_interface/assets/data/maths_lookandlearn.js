d58f548848e4260963c614bc8dd9f9b8f3662814
Cyprien <cyprien.larrouy@gmail.com>
2016-12-27 07:57:46 +0100

    minigame tester for lookandlearn maths

---
+++ app/modules/minigame_tester_interface/assets/data/maths_lookandlearn.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "discipline" : "maths",
+    "data" : {
+        "notions" : [
+            {
+                id    : 50,
+                value : 50,
+                image : 'assets/images/maths/number_50.png',
+                sound : 'assets/sounds/maths/number_50.ogg'
+            },
+            {
+                id    : 51,
+                value : 51,
+                image : 'assets/images/maths/number_51.png',
+                sound : 'assets/sounds/maths/number_51.ogg'
+            },
+            {
+                id    : 52,
+                value : 52,
+                image : 'assets/images/maths/number_52.png',
+                sound : 'assets/sounds/maths/number_52.ogg'
+            },
+            {
+                id    : 53,
+                value : 53,
+                image : 'assets/images/maths/number_53.png',
+                sound : 'assets/sounds/maths/number_53.ogg'
+            },
+            {
+                id    : 54,
+                value : 54,
+                image : 'assets/images/maths/number_54.png',
+                sound : 'assets/sounds/maths/number_54.ogg'
+            }
+        ]
+    }
+};
