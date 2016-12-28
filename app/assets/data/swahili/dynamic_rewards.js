27443bed90809fb3a54e1c99d6166a74e3fcfd13
Cyprien Larrouy <clarrouy@manzalab.com>
2016-12-28 12:36:05 +0100

    integration rewards spots generation

---
+++ app/assets/data/swahili/dynamic_rewards.js
@@ -0,0 +1,83 @@
+define([], function () {
+    'use strict';
+    function Reward () {
+        this.levelRewards = {
+    "language": {
+        "1": "Mamba_mwenye_njaa",
+        "5": "Kitabu_cha_Hali_ya_Hewa",
+        "9": "Maharagwe_Yanayomea",
+        "13": "Meno",
+        "17": "I_like_to_read",
+        "19": "Wanyama_wa_kufugwa",
+        "23": "Bi_Simu",
+        "25": "Happy_and_sad",
+        "27": "Siku_Upinde_ulitembelea_kijiji",
+        "31": "Marafiki",
+        "33": "Let_s_go",
+        "35": "Upepo",
+        "41": "Little_and_big",
+        "49": "A_very_tall_man",
+        "57": "Houses",
+        "65": "Counting_animals",
+        "73": "Feet_hands_and_head_"
+    },
+    "maths": {
+        "1": "Akili1",
+        "9": "Akili2",
+        "10": "CIRKAFRICA1 MASTER PARTIE 03",
+        "16": "Akili3",
+        "19": "CIRKAFRICA1 MASTER PARTIE 05",
+        "23": "Akili4",
+        "30": "CIRKAFRICA1 MASTER PARTIE 07",
+        "34": "Akili6",
+        "42": "CIRKAFRICA1 MASTER PARTIE 09",
+        "43": "Akili8"
+    },
+    "both": [
+        "CIRKAFRICA1 MASTER PARTIE 02",
+        "CIRKAFRICA1 MASTER PARTIE 04",
+        "CIRKAFRICA1 MASTER PARTIE 06",
+        "CIRKAFRICA1 MASTER PARTIE 08",
+        "patrimath",
+        "CIRKAFRICA1 MASTER PARTIE 01",
+        "CIRKAFRICA1 MASTER PARTIE 02",
+        "CIRKAFRICA1 MASTER PARTIE 03",
+        "CIRKAFRICA1 MASTER PARTIE 04",
+        "patrimath"
+    ],
+    "bookCount": 10,
+    "videoCount": 10,
+    "gameCount": 1,
+    "locked": {
+        "game": [
+            "patrimath"
+        ],
+        "video": [
+            "Akili1",
+            "Akili2",
+            "CIRKAFRICA1 MASTER PARTIE 01",
+            "Akili3",
+            "CIRKAFRICA1 MASTER PARTIE 02",
+            "Akili4",
+            "CIRKAFRICA1 MASTER PARTIE 03",
+            "Akili6",
+            "CIRKAFRICA1 MASTER PARTIE 04",
+            "Akili8"
+        ],
+        "book": [
+            "Mamba_mwenye_njaa",
+            "Kitabu_cha_Hali_ya_Hewa",
+            "Maharagwe_Yanayomea",
+            "Meno",
+            "Wanyama_wa_kufugwa",
+            "Bi_Simu",
+            "Siku_Upinde_ulitembelea_kijiji",
+            "Marafiki",
+            "Mamangu_alipanda",
+            "Upepo"
+        ]
+    }
+}
+    };
+    return new Reward(); 
+});
---
+++ npm_scripts/compute_rewards_spots.js
@@ -0,0 +1,115 @@
+(function computeRewardsSpots () {
+
+    'use strict';
+
+    var fs = require('fs');
+    var d3 = require('d3-dsv');
+    
+    var levelRewards = { language:{}, maths:{}, both:[], bookCount:0, videoCount:0, gameCount:0 };
+
+    setRewardsSpots('english');
+    setRewardsSpots('swahili');
+
+    function setRewardsSpots (language) {
+
+        var lRewardsCSV = d3.dsvFormat(",").parse(fs.readFileSync('app/assets/data/' + language + '/rewards.csv', 'UTF-8'));
+        
+        var lRewardCount_language = 0;
+        var lRewardCount_maths = 0;
+        
+        var lRewards_language = [];
+        var lRewards_maths = [];
+        
+        var lBookCount = 0, lVideoCount = 0, lGameCount = 0;
+        var lLockedGames = [], lLockedVideos = [], lLockedBooks = [];
+
+        for (var i = 0; i < lRewardsCSV.length; i++)
+        {
+            if (lRewardsCSV[i].BOTH)   levelRewards.both.push(lRewardsCSV[i]["FOLDER NAME"]);
+            else if (lRewardsCSV[i].LANGUAGE)  lRewardCount_language++;
+            else  lRewardCount_maths++;
+
+            if (lRewardsCSV[i].TYPE === "book")
+            {
+                lBookCount++;
+                lLockedBooks.push(lRewardsCSV[i]["FOLDER NAME"]);
+            } 
+            else if (lRewardsCSV[i].TYPE === "video") 
+            {
+                lVideoCount++;
+                lLockedVideos.push(lRewardsCSV[i]["FOLDER NAME"]);
+            }
+            else
+            {
+                lGameCount++;
+                lLockedGames.push(lRewardsCSV[i]["FOLDER NAME"]);
+            } 
+
+            if (lRewardsCSV[i].BOTH) continue;
+            var lRewards = lRewardsCSV[i].LANGUAGE?lRewards_language:lRewards_maths;
+            lRewards.push(lRewardsCSV[i]["FOLDER NAME"]);
+        }
+        levelRewards.bookCount = lBookCount;
+        levelRewards.videoCount = lVideoCount;
+        levelRewards.gameCount = lGameCount;
+        levelRewards.locked = {
+            game: lLockedGames, 
+            video: lLockedVideos,
+            book: lLockedBooks
+        };
+
+        var lPlan_language = d3.dsvFormat(";").parse(fs.readFileSync('app/modules/language/assets/data/' + language + '/plan.csv', 'UTF-8'));
+        var lPlan_maths    = d3.dsvFormat(";").parse(fs.readFileSync('app/modules/maths/assets/data/plan.csv', 'UTF-8'));
+
+        for (var a = 0; a < 2; a++) 
+        {
+            var lPlan        = a === 0 ? lPlan_language:lPlan_maths;
+            var lLevelCount  = lPlan.length;
+            var lRewardCount = a === 0 ? lRewardCount_language:lRewardCount_maths;
+            var lRewards     = a === 0 ? lRewards_language:lRewards_maths;
+            var lChapters    = [];
+            var lStepForReward;
+
+            for (var i = 0; i < lLevelCount; i++) 
+            {
+                var lLessons = [];
+                for (var j = i; j < lLevelCount; j++) 
+                {
+                    if (lPlan[j].CHAPTER == lPlan[i].CHAPTER) lLessons.push(lPlan[j].LESSON);
+                    else {
+                        i+=lLessons.length-1;
+                        break;
+                    }
+                }
+                if (lChapters.length<lPlan[lLevelCount-1].CHAPTER) lChapters.push(lLessons);
+            }
+
+            for (var i = 0; i < lChapters.length; i++) 
+            {
+                var lChapterLessons = lChapters[i];
+                var lLessonSemiBonus = lChapterLessons[Math.floor(lChapterLessons.length/2)];
+                lChapterLessons.splice(lChapterLessons.indexOf(lLessonSemiBonus), 1);
+            }
+
+            lChapters = Array.prototype.concat.apply([], lChapters);
+            lStepForReward = Math.round(lChapters.length / lRewardCount);
+            for (var j = 0; j < lRewardCount; j++) 
+            {
+                var lIndex = Math.min(lChapters[Math.min(j*lStepForReward,lChapters.length-1)],lLevelCount);
+                (a === 0 ? levelRewards.language : levelRewards.maths)[lIndex] = lRewards.shift();
+            }
+
+        }
+
+        var str =   "define([], function () {\n"+
+                    "    'use strict';\n"+
+                    "    function Reward () {\n"+
+                    "        this.levelRewards = "+JSON.stringify(levelRewards, null, "    ") + "\n" +
+                    "    };\n"+
+                    "    return new Reward(); \n"+
+                    "});";
+        
+        fs.writeFile('app/assets/data/' + language + '/dynamic_rewards.js', str);
+    }
+
+})();
