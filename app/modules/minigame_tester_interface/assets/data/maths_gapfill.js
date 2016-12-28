6101cba41ee3e7250bef4a7f9ce5bc82d1157e3e
Cyprien Larrouy <clarrouy@manzalab.com>
2016-12-28 17:58:26 +0100

    various minor corrections

--- app/modules/minigame_tester_interface/assets/data/maths_gapfill.js
+++ app/modules/minigame_tester_interface/assets/data/maths_gapfill.js
@@ -1,97 +1,169 @@
-
 module.exports = {
-
-    discipline : 'maths',
-
-    language   : 'english',
-
-    data: {
-        rounds: [
-            {
-                steps: [{
-                    stimuli: [
-                        {
-                            "value": "3",
-                            "soundPath": "assets/sounds/maths/number_3.ogg",
-                            "notionId": "3",
-                        },
-                        {
-                            "value": "4",
-                            "soundPath": "assets/sounds/maths/number_4.ogg",
-                            "notionId": "2",
-                        },
-                        {
-                            "value": "5",
-                            "soundPath": "assets/sounds/maths/number_5.ogg",
-                            "notionId": "2",
-                        },
-                    ],
-                }]
-            },
-            {
-                steps: [{
-                    stimuli: [
-                        {
-                            "value": "1",
-                            "soundPath": "assets/sounds/maths/number_1.ogg",
-                            "notionId": "3",
-                        },
-                        {
-                            "value": "2",
-                            "soundPath": "assets/sounds/maths/number_2.ogg",
-                            "notionId": "2",
-                        },
-                        {
-                            "value": "5",
-                            "soundPath": "assets/sounds/maths/number_5.ogg",
-                            "notionId": "2",
-                        },
-                    ],
-                }]
-            },
-            {
-                steps: [{
-                    stimuli: [
-                        {
-                            "value": "3",
-                            "soundPath": "assets/sounds/maths/number_3.ogg",
-                            "notionId": "3",
-                        },
-                        {
-                            "value": "4",
-                            "soundPath": "assets/sounds/maths/number_4.ogg",
-                            "notionId": "2",
-                        },
-                        {
-                            "value": "5",
-                            "soundPath": "assets/sounds/maths/number_5.ogg",
-                            "notionId": "2",
-                        },
-                    ],
-                }]
-            },
-            {
-                steps: [{
-                    stimuli: [
-                        {
-                            "value": "3",
-                            "soundPath": "assets/sounds/maths/number_3.ogg",
-                            "notionId": "3",
-                        },
-                        {
-
-                            "value": "5",
-                            "soundPath": "assets/sounds/maths/number_5.ogg",
-                            "notionId": "2",
-                        },
-                        {
-                            "value": "4",
-                            "soundPath": "assets/sounds/maths/number_4.ogg",
-                            "notionId": "2",
-                        },
-                    ],
-                }]
-            }
+  "discipline": "maths",
+  "language": "english",
+  "data": {
+    "rounds": [
+      {
+        "steps": [
+          {
+            "stimuli": [
+              {
+                "id": "1",
+                "value": "1",
+                "soundPath": "assets/sounds/maths/number_1.ogg"
+              },
+              {
+                "id": "8",
+                "value": "8",
+                "soundPath": "assets/sounds/maths/number_8.ogg"
+              },
+              {
+                "id": "5",
+                "value": "5",
+                "soundPath": "assets/sounds/maths/number_5.ogg"
+              }
+            ]
+          }
         ]
-    }
+      },
+      {
+        "steps": [
+          {
+            "stimuli": [
+              {
+                "id": "2",
+                "value": "2",
+                "soundPath": "assets/sounds/maths/number_2.ogg"
+              },
+              {
+                "id": "9",
+                "value": "9",
+                "soundPath": "assets/sounds/maths/number_9.ogg"
+              },
+              {
+                "id": "6",
+                "value": "6",
+                "soundPath": "assets/sounds/maths/number_6.ogg"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "steps": [
+          {
+            "stimuli": [
+              {
+                "id": "3",
+                "value": "3",
+                "soundPath": "assets/sounds/maths/number_3.ogg"
+              },
+              {
+                "id": "10",
+                "value": "10",
+                "soundPath": "assets/sounds/maths/number_10.ogg"
+              },
+              {
+                "id": "7",
+                "value": "7",
+                "soundPath": "assets/sounds/maths/number_7.ogg"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "steps": [
+          {
+            "stimuli": [
+              {
+                "id": "4",
+                "value": "4",
+                "soundPath": "assets/sounds/maths/number_4.ogg"
+              },
+              {
+                "id": "1",
+                "value": "1",
+                "soundPath": "assets/sounds/maths/number_1.ogg"
+              },
+              {
+                "id": "8",
+                "value": "8",
+                "soundPath": "assets/sounds/maths/number_8.ogg"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "steps": [
+          {
+            "stimuli": [
+              {
+                "id": "5",
+                "value": "5",
+                "soundPath": "assets/sounds/maths/number_5.ogg"
+              },
+              {
+                "id": "2",
+                "value": "2",
+                "soundPath": "assets/sounds/maths/number_2.ogg"
+              },
+              {
+                "id": "9",
+                "value": "9",
+                "soundPath": "assets/sounds/maths/number_9.ogg"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "steps": [
+          {
+            "stimuli": [
+              {
+                "id": "6",
+                "value": "6",
+                "soundPath": "assets/sounds/maths/number_6.ogg"
+              },
+              {
+                "id": "3",
+                "value": "3",
+                "soundPath": "assets/sounds/maths/number_3.ogg"
+              },
+              {
+                "id": "10",
+                "value": "10",
+                "soundPath": "assets/sounds/maths/number_10.ogg"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "steps": [
+          {
+            "stimuli": [
+              {
+                "id": "7",
+                "value": "7",
+                "soundPath": "assets/sounds/maths/number_7.ogg"
+              },
+              {
+                "id": "4",
+                "value": "4",
+                "soundPath": "assets/sounds/maths/number_4.ogg"
+              },
+              {
+                "id": "1",
+                "value": "1",
+                "soundPath": "assets/sounds/maths/number_1.ogg"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
 };
--- app/modules/minigame_tester_interface/src/index.js
+++ app/modules/minigame_tester_interface/src/index.js
@@ -29,7 +29,7 @@
 
         this.options =  {
             LANGUAGE    : KALULU_LANGUAGE, // cannot be changed post build, presence for information only.
-            minigame    : 'lookandlearn',
+            minigame    : 'ants',
             discipline  : 'maths',
             globalLevel : 1,
             localLevel  : 1,
