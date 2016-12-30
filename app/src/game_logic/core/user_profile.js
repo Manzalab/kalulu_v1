e55bf0ae77bc75d9605c360e10f627e92c9d1a5e
benjamin <bpageaud@manzalab.com>
2016-12-30 16:32:44 +0100

    modif usersave

--- app/src/game_logic/core/user_profile.js
+++ app/src/game_logic/core/user_profile.js
@@ -39,23 +39,6 @@ define([
 
         if (!this._data) this._createSave();
         if (Config.enableGlobalVars) window.kalulu.userProfile = this;
-        if (!this._data.kaluluTalks) this._data.kaluluTalks = {
-                brainScreen : true,
-                firstTreasure : true,
-                gardenScreen : true,
-                gardenLetter : true,
-                gardenLesson : true,
-                firstPlantEvolve : true,
-                lastPlantEvolve : true,
-                gardenPlant : true,
-                firstStar : true,
-                lesson1 : true,
-                lessonGame1 : true,
-                lessonGame2 : true,
-                lesson2 : true,
-                toyChestScreen : true,
-                lastReward : ""
-        };
     }
 
     //AddUserRecord(UserProfile);
@@ -181,7 +164,24 @@ define([
             bonusActivitiesData : null,
             starMiddle : null,
             fertilizer : 0,
-            plantsData : null
+            plantsData : null,
+            kaluluTalks : {
+                brainScreen : true,
+                firstTreasure : true,
+                gardenScreen : true,
+                gardenLetter : true,
+                gardenLesson : true,
+                firstPlantEvolve : true,
+                lastPlantEvolve : true,
+                gardenPlant : true,
+                firstStar : true,
+                lesson1 : true,
+                lessonGame1 : true,
+                lessonGame2 : true,
+                lesson2 : true,
+                toyChestScreen : true,
+                lastReward : ""
+            }
         };
         this.getStarMiddle();
         this.getPlantsStatus();
