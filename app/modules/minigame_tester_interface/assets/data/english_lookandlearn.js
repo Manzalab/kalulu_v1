dc443d59083a8a3bf50c390721fd21d7e6d8b252
Cyprien Larrouy <clarrouy@manzalab.com>
2017-01-05 14:44:22 +0100

    typo correction

--- app/modules/minigame_tester_interface/assets/data/english_lookandlearn.js
+++ app/modules/minigame_tester_interface/assets/data/english_lookandlearn.js
@@ -8,7 +8,7 @@ module.exports = {
                 "value"             : "b",
                 "textValue"         : "b",
                 "video1"            : "assets/video/language/phoneme_b.mp4",
-                "video2"            : "assets/video/language/phone_b_closeup.mp4",
+                "video2"            : "assets/video/language/phoneme_b_closeup.mp4",
                 "sound"             : "assets/sounds/language/phoneme_b.ogg",
                 "image"             : "assets/images/language/lookandlearn/bottle.jpg",
                 "illustrativeSound" : "assets/sounds/language/phoneme_illustration_bottle.ogg",
