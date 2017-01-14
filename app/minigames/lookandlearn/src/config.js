8ea574aa3beb51c3ff7b4d3de8f9dc59886bcf3f
Cyprien <cyprien.larrouy@gmail.com>
2017-01-14 21:23:49 +0100

    synchronous config require

--- app/minigames/lookandlearn/src/config.js
+++ app/minigames/lookandlearn/src/config.js
@@ -1,64 +1,46 @@
-var staticCallback = null;
+(function () {
 
-function MinigameConfig () {
+    'use strict';
 
-    this.gameId          = "";     // the module build will export to this foldername. lowercase for the filesystem
-    this.skipKalulu      = false; // do not play kalulu speeches for faster debug. incomplete implementation
-    this.skipKaluluIntro = false; // do not play kalulu speeches for faster debug. incomplete implementation
-    this.skipKaluluHelp  = false; // do not play kalulu speeches for faster debug. incomplete implementation
-    this.skipKaluluFinal = false; // do not play kalulu speeches for faster debug. incomplete implementation
-    this.debugPanel      = false;
-    this.globalVars      = false;
-    this.debugPencil     = false;
-    this.debugLayouts    = false;
-    this.initDone        = false;
-    this.disciplines     = {
-        language    : "reading",
-        arithmetics : "maths",
-        geometry    : "geo"
-    };
-}
-
-MinigameConfig.prototype.requestMinigameConfig = function requestMinigameConfig (callback) {
+    var config = require('minigames/lookandlearn/assets/config/config.json');
     
-    console.info("[Minigame Look And Learn] Requesting config file");
-    staticCallback = callback;
-    this.httpRequest = new XMLHttpRequest();
+    function MinigameConfig () {
 
-    if (!this.httpRequest) {
-      alert('Giving up =( Cannot create an XMLHTTP instance');
-      return false;
+        this.gameId          = "";     // the module build will export to this foldername. lowercase for the filesystem
+        this.skipKalulu      = false; // do not play kalulu speeches for faster debug. incomplete implementation
+        this.skipKaluluIntro = false; // do not play kalulu speeches for faster debug. incomplete implementation
+        this.skipKaluluHelp  = false; // do not play kalulu speeches for faster debug. incomplete implementation
+        this.skipKaluluFinal = false; // do not play kalulu speeches for faster debug. incomplete implementation
+        this.debugPanel      = false;
+        this.globalVars      = false;
+        this.debugPencil     = false;
+        this.debugLayouts    = false;
+        this.initDone        = false;
+        this.disciplines     = {
+            language    : "reading",
+            arithmetics : "maths",
+            geometry    : "geo"
+        };
+
+        this._init(config);
     }
 
-    this.httpRequest.onreadystatechange = this.onStateChange.bind(this);
-    this.httpRequest.open('GET', 'minigames/lookandlearn/assets/config/config.json');
-    this.httpRequest.send();
-};
+    MinigameConfig.prototype._init = function initConfig (config) {
+        
+        this.gameId          = config.gameId;
+        this.skipKalulu      = config.skipKalulu;
+        this.skipKaluluIntro = config.skipKaluluIntro;
+        this.skipKaluluHelp  = config.skipKaluluHelp;
+        this.skipKaluluFinal = config.skipKaluluFinal;
+        this.debugPanel      = config.debugPanel;
+        this.globalVars      = config.globalVars;
+        this.debugPencil     = config.debugPencil;
+        this.debugLayouts    = config.debugLayouts;
+        this.initDone        = true;
+    };
 
-MinigameConfig.prototype.onStateChange = function onStateChange () {
-    if (this.httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (this.httpRequest.status === 200) {
-        console.info(this.httpRequest);
-        this.init(JSON.parse(this.httpRequest.responseText));
-      } else {
-        console.error('There was a problem with the request.');
-      }
-    }
-};
+    module.exports = new MinigameConfig();
+})();
 
-MinigameConfig.prototype.init = function init (config) {
-    console.log(config);
-    this.gameId          = config.gameId;
-    this.skipKalulu      = config.skipKalulu;
-    this.skipKaluluIntro = config.skipKaluluIntro;
-    this.skipKaluluHelp  = config.skipKaluluHelp;
-    this.skipKaluluFinal = config.skipKaluluFinal;
-    this.debugPanel      = config.debugPanel;
-    this.globalVars      = config.globalVars;
-    this.debugPencil     = config.debugPencil;
-    this.debugLayouts    = config.debugLayouts;
-    this.initDone        = true;
-    staticCallback();
-};
 
-module.exports = new MinigameConfig();
+
